fcc7b3ec38f2cebce71c01aa3bac543563199cea
Joth Ending <jothending@gmail.com>
2022-01-21 11:34:47 -0500

    test

---
+++ static/index.js
@@ -0,0 +1,8 @@
+function spoilerToggle(id) {
+    el = document.getElementById(id)
+    if (el.style.display == "none") {
+        el.style.display = "block"
+    } else {
+        el.style.display = "none"
+    }
+}
